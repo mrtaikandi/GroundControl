9d9bfb4849f5b41abe890e09ed43db685e714fa8
Mohammadreza Taikandi <mrtaikandi@hotmail.com>
2026-04-03 15:28:38 +0100

    feat(cli): add HTTP client resiliency

--- src/GroundControl.Cli/Program.cs
+++ src/GroundControl.Cli/Program.cs
@@ -1,2 +1,12 @@
+using GroundControl.Api.Client;
+using Microsoft.Extensions.DependencyInjection;
 
-await new CliHostBuilder(args, "GroundControl management tool").RunAsync();
+var builder = new CliHostBuilder(args, "GroundControl management tool");
+builder.Services.AddGroundControlClient(options =>
+    {
+        var baseAddress = builder.Configuration["GroundControl:ServerUrl"] ?? "https://localhost:5001";
+        options.BaseAddress = new Uri(baseAddress);
+    })
+    .AddStandardResilienceHandler();
+
+await builder.RunAsync();
