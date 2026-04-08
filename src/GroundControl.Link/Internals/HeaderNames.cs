780f1229dd3ab708a362fc5d8aebd28d703dfceb
Mohammadreza Taikandi <mrtaikandi@hotmail.com>
2026-04-08 07:18:01 +0100

    refactor(link): use HeaderNames.ApiKey for authorization header

--- src/GroundControl.Link/Internals/HeaderNames.cs
+++ src/GroundControl.Link/Internals/HeaderNames.cs
@@ -2,6 +2,7 @@ namespace GroundControl.Link.Internals;
 
 internal static class HeaderNames
 {
+    public const string ApiKey = "ApiKey";
     public const string ApiVersion = "api-version";
     public const string LastEventId = "Last-Event-ID";
 }
